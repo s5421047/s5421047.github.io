f58d6580b4595927baf6b13105399e5d0bc73d3b
s5421047 <114902569+s5421047@users.noreply.github.com>
2022-10-18 20:45:35 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-quantum-logo-2017.jpg') {
+    myImage.setAttribute ('src','images/unnamed.png');
+  } else {
+    myImage.setAttribute ('src','images/firefox-quantum-logo-2017.jpg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+ 
